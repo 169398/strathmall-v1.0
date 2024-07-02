9fd0b822b6082b481f5eeec00e0be3110fd56f71
idris <kulubiidris@gmail.com>
2024-07-02 18:08:41 +0300

    files

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import React from "react";
 export default function page() {
 	return (
 		<div className="p-5 flex items-center justify-center h-screen flex-col">
-			<h1 className="text-4xl">Welcome to SupaAuth</h1>
+			<h1 className="text-4xl">Welcome to StrathMall</h1>
 <Navbar />
 		</div>
 	);
